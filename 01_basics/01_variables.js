b54dbb1d8b39c597722f9e6e5f8a63f74bf24603
Saumya Tiwari <73695360+saumyatiwaryy@users.noreply.github.com>
2023-09-18 11:08:31 +0000

    data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,9 @@
 const accountId=969659
 let accountName="Saumya"
+// accountId = 2 // not allowed
  let accountState;
- console.table([accountId,accountName,accountState])    
+ console.table([accountId,accountName,accountState]) 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
